f32c1546191fea9bd5ce60bb05e3bb8f54096596
tom <tomphill@live.co.uk>
2023-02-02 12:26:02 +0800

    update index page

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,3 @@
-import Head from 'next/head';
-
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>BlogStandard - AI Generated Blog Posts</title>
-        <meta
-          name="description"
-          content="BlogStandard - The AI-powered SAAS solution to generate SEO-optimized blog posts in minutes. Get high-quality content, without sacrificing your time."
-        />
-        <link rel="icon" href="/favicon.png" />
-      </Head>
-    </div>
-  );
+  return <div />;
 }
