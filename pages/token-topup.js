d70300b41ce4b26d61d0044b019b99ab97300fce
leilafaez <leila.faez@gmail.com>
2023-10-20 13:01:45 +0100

    implement protected routes that are only accessible if logged in

--- pages/token-topup.js
+++ pages/token-topup.js
@@ -1,3 +1,15 @@
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+
 export default function TokenTopUp() {
-    return <div><h1>Token</h1></div>;
-  }
+  return (
+    <div>
+      <h1>Token</h1>
+    </div>
+  );
+}
+
+export const getServerSideProps = withPageAuthRequired(() => {
+  return {
+    props: {},
+  };
+});
