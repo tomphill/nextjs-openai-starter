0f66aa9502ac408ae244184c4e5f79fe1f6ac7ee
leilafaez <leila.faez@gmail.com>
2023-10-31 20:43:07 +0000

    create the homepage/main landing page and finish off the sidebar header

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -15,7 +15,7 @@ export const AppLayout=({children})=>{
          <Logo/>
          <Link
            href="/post/new"
-           className="bg-green-500 tracking-wider w-full text-center text-white font-bold cursor-pointer uppercase px-4 py-2 rounded-md hover:bg-green-600 transition-colors block"
+           className="btn"
          >
            New Post
          </Link>
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
+import Image from "next/image";
+import HeroImage from "../public/hero.webp";
+import { Logo } from "../components/Logo";
+import Link from "next/link";
 
 export default function Home() {
   
-
-  
   return (
-    <div>
-      
+    <div className="w-screen h-screen overflow-hidden flex justify-center items-center relative">
+      <Image src={HeroImage} alt="Hero" fill className="absolute"/>
+      <div className="relative z-10 text-white px-10 py-5 text-center max-w-screen-sm bg-slate-900/90 rounded-md backdrop-blur-sm">
+         <Logo/>
+         <p>
+          The AI-powered SAAS solution to generate SEO-optimized blog posts in minutes.
+          Get high-quality content, without sacrificing your time.
+         </p>
+         <Link href="/post/new" className="btn">Begin</Link>
+      </div>
     </div>
   );
 }
