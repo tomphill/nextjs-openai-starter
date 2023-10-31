b368f24781b18efe0fdbfb748fc9b5edc6b09bd9
leilafaez <leila.faez@gmail.com>
2023-10-31 20:14:21 +0000

    create the app logo component using google fonts and fontAwesome

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -3,6 +3,7 @@ import {useUser} from "@auth0/nextjs-auth0/client";
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCoins } from "@fortawesome/free-solid-svg-icons";
+import { Logo } from "../Logo";
 
 
 export const AppLayout=({children})=>{
@@ -11,7 +12,7 @@ export const AppLayout=({children})=>{
    <div className="grid grid-cols-[300px_1fr] h-screen max-h-screen">
      <div className="flex flex-col text-white overflow-hidden">
        <div className="bg-slate-800 px-2">
-         <div>logo</div>
+         <Logo/>
          <Link
            href="/post/new"
            className="bg-green-500 tracking-wider w-full text-center text-white font-bold cursor-pointer uppercase px-4 py-2 rounded-md hover:bg-green-600 transition-colors block"
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,23 @@
-import '../styles/globals.css'
-import { UserProvider } from '@auth0/nextjs-auth0/client'
+import '../styles/globals.css';
+import { UserProvider } from '@auth0/nextjs-auth0/client';
+import {DM_Sans,DM_Serif_Display} from "@next/font/google";
+
+const dmSans = DM_Sans({
+  weight : ["400","500","700"],
+  subsets :["latin"],
+  variable: "--font-dm-sans"
+});
+const dmSerifDisplay = DM_Serif_Display({
+  weight: ["400"],
+  subsets: ["latin"],
+  variable: "--font-dm-serif",
+});
 
 function MyApp({ Component, pageProps }) {
   const getLayout=Component.getLayout || ((page)=>page)
   return (
     <UserProvider>
-    {getLayout(<Component {...pageProps} />,pageProps)}
+      <main className={`${dmSans.variable} ${dmSerifDisplay.variable} font-body`}> {getLayout(<Component {...pageProps} />, pageProps)}</main>
     </UserProvider>
   );
 }
